6fffee2735fcf9e0e135f348916d0cb71a071bfd
Taarini1 <98458225+Taarini1@users.noreply.github.com>
2023-05-26 13:46:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+  mario_jump = loadSound("jump.wav");
+  mario_step_on_enemy=loadSound("kick.wav");
+  mario_coin = loadSound("coin.wav");
+  mario_die = loadSound("mariodie.wav");
+  gameover= loadSound("gameover.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	
+	canvas.parent('canvas');
+	
+	instializeInSetup(mario);
+
+	
+
+	video = createCapture(VIDEO);
+  video.size(600,300);
+  video.parent("game_console");
+
+  poseNet=ml5.poseNet(video,modelLoaded);
+  poseNet.on("pose",gotPoses)
+
+
+	
+}
+
+function modelLoaded(){
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log(results) 
+}
+}
+
+function draw() {
+	game();
+}
+
+
+
+
+
+
